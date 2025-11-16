3265cba8475c572cb5de5a9f9f92bb9aa3c49bb6
sweetburble <sweetburble2716@gmail.com>
2025-11-16 09:23:31 +0900

    실행파일 로직 변경

--- [Train] execute_file.py
+++ [Train] execute_file.py
@@ -11,7 +11,6 @@ import subprocess
 #     "--image_size 256 "
 #     "--c_dim 5 "
 #     "--selected_attrs Black_Hair Blond_Hair Brown_Hair Male Young "
-#     "--model_save_dir=stargan_celeba_256/models "
 #     "--result_dir=result_test "
 #     "--test_iters 200000 "
 #     "--batch_size 1"
